a0deeecfd7f6214e42ff2a91d04035f9bfa26575
Muhammad Khoshnaw <khoshnaw.eng@gmail.com>
2022-02-19 22:04:28 +0300

    Improving test (#29)
    * fixing tests
    
    Co-authored-by: MuhammadXoshnaw <muhammad.xoshnaw.eng@gmail.com>

--- viewModel/src/test/java/com/khoshnaw/viewmodel/movies/MoviesViewModelTest.kt
+++ viewModel/src/test/java/com/khoshnaw/viewmodel/movies/MoviesViewModelTest.kt
@@ -1,15 +1,29 @@
 package com.khoshnaw.viewmodel.movies
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.viewModelScope
 import com.khoshnaw.controller.movie.MovieController
+import com.khoshnaw.viewmodel.util.CoroutineTestRule
 import io.mockk.MockKAnnotations
 import io.mockk.coVerify
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 
+@ExperimentalCoroutinesApi
 class MoviesViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutinesDispatcherRule = CoroutineTestRule()
 
     @MockK
     lateinit var movieController: MovieController
@@ -18,10 +32,13 @@ class MoviesViewModelTest {
     lateinit var viewModel: MoviesViewModel
 
     @Before
-    fun setup() = MockKAnnotations.init(this, relaxUnitFun = true)
+    fun setup() {
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        viewModel.viewModelScope
+    }
 
     @Test
-    fun `use movie controller to refresh movies`() = runBlocking {
+    fun `use movie controller to refresh movies`() = runTest(StandardTestDispatcher()) {
         viewModel.intents.send(MoviesIntent.RefreshMovies)
 
         coVerify { movieController.loadMoviesList() }
